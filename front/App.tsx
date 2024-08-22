a56f0000524ebbedd2103ee690c86e76ab236e35
hvihvi <hugo.vinet@gmail.com>
2024-08-22 17:56:19 +0200

    query hello world from server

--- front/App.tsx
+++ front/App.tsx
@@ -1,14 +1,40 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+import { Text, StyleSheet, View } from "react-native"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:5000/graphql',
+  cache: new InMemoryCache(),
+});
+
+const HELLO_QUERY = gql`
+  query GetHello {
+    hello
+  }
+`;
+
+const Hello = () => {
+  const { loading, error, data } = useQuery(HELLO_QUERY);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error :(</Text>;
+
+  return <View style={styles.container}><Text>{data.hello}</Text></View>;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <ApolloProvider client={client}>
+      <Hello />
       <StatusBar style="auto" />
-    </View>
+    </ApolloProvider>
   );
 }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
